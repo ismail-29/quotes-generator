4aa8f050755d2ab80a51a56d3aa0b810528c1cb9
ismail-29 <esmailpasha200@gmail.com>
2023-05-11 14:05:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      });
+  };
+
+  const handleClick = () => {
+    getQuote();
+  };
+
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="quote-box">
+        <div id="text">{quote}</div>
+        <div id="author">{author}</div>
+        <div className="button-container">
+          <a
+            href={twitterUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            id="tweet-quote"
+          ><i class="fab fa-twitter">tweet</i>
+
+          </a>
+          <button id="new-quote" onClick={handleClick}>
+            New Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
